0398861beba53a23eecc7aabcc06b4cc7a6f4f91
Corey Goldstein <coreybgoldstein@gmail.com>
2026-05-01 19:02:13 -0400

    fix Package.swift to use remote plugin URLs for Xcode Cloud

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 5.9
 import PackageDescription
-
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
     name: "CapApp-SPM",
@@ -12,11 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.0"),
-        .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera"),
-        .package(name: "CapacitorGeolocation", path: "../../../node_modules/@capacitor/geolocation"),
-        .package(name: "CapacitorPushNotifications", path: "../../../node_modules/@capacitor/push-notifications"),
-        .package(name: "CapacitorSplashScreen", path: "../../../node_modules/@capacitor/splash-screen"),
-        .package(name: "CapacitorStatusBar", path: "../../../node_modules/@capacitor/status-bar")
+        .package(url: "https://github.com/ionic-team/capacitor-plugins.git", exact: "8.0.2")
     ],
     targets: [
         .target(
@@ -24,11 +19,11 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "CapacitorCamera", package: "CapacitorCamera"),
-                .product(name: "CapacitorGeolocation", package: "CapacitorGeolocation"),
-                .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications"),
-                .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen"),
-                .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar")
+                .product(name: "CapacitorCamera", package: "capacitor-plugins"),
+                .product(name: "CapacitorGeolocation", package: "capacitor-plugins"),
+                .product(name: "CapacitorPushNotifications", package: "capacitor-plugins"),
+                .product(name: "CapacitorSplashScreen", package: "capacitor-plugins"),
+                .product(name: "CapacitorStatusBar", package: "capacitor-plugins")
             ]
         )
     ]
