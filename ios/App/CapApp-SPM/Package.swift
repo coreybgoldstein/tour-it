f341965c702a66363dba8707ee80e6df701945d6
Corey Goldstein <coreybgoldstein@gmail.com>
2026-05-01 23:35:27 -0400

    update Package.swift with branch references

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -1,17 +1,14 @@
 // swift-tools-version: 5.9
 import PackageDescription
-// DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
     name: "CapApp-SPM",
     platforms: [.iOS(.v15)],
     products: [
-        .library(
-            name: "CapApp-SPM",
-            targets: ["CapApp-SPM"])
+        .library(name: "CapApp-SPM", targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.0"),
-        .package(url: "https://github.com/ionic-team/capacitor-plugins.git", from: "1.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "8.3.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-plugins.git", branch: "@capacitor/camera@8.0.2")
     ],
     targets: [
         .target(
